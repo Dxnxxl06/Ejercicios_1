e26b56af886e78a27c8b302a71aa13a725401dbc
Daniel06 <danielplazas1929@gmail.com>
2025-06-05 10:45:58 -0500

    Actividad completa

--- Taller 5/main.py
+++ Taller 5/main.py
@@ -1,45 +1,100 @@
 import json
-Bodega = "Taller 5/productos.json"
+fetch = "Taller 5/productos.json"
+almacen = [
+    {
+        "id": 1,
+        "name": "Laptop",
+        "price": 1000
+    },
+    {
+        "id": 2,
+        "name": "Monitor",
+        "price": 400
+    },
+    {
+        "id": 3,
+        "name": "Mouse",
+        "price": 25
+    }
 
-productos= {
-    "productos": [
-        {
-            "id": 1,
-            "nombre": "Mouse",
-            "precio": 10
-        },
-        {
-            "id": 2,
-            "nombre": "Pantalla",
-            "precio": 50
-        },
-        {
-            "id": 3,
-            "nombre": "Teclado",
-            "precio": 20
-        }
-    ]
-}
+]
 
-with open(Bodega, 'w', encoding="utf-8" ) as file:
-    json.dump(productos, file, indent=2)
 
-try :
-    
-    with open(Bodega, "r", enconding="utf-8" ) as file:
-        datos = json.load(file)
 
-    nuevo_producto = {"id": 4, "nombre": "Webcam", "precio": 40}
-    datos["productos"].append(nuevo_producto)
+def saveProducts():
+    with open(fetch, "w") as file:
+        json.dump(almacen, file, indent=2)
 
-    with open(Bodega, "w", encoding="utf-8") as file:
-        json.dump(productos, file, indent=2)
+def addProduct():
+    with open(fetch, "r") as file:
+        products = json.load(file)
+        print(products)
 
-    print("Productos agregado correctamente")
+    for i in range(len(products)):
+        id = int(input("Input pruduct ID: "))
+        name = str(input("Input product name: "))
+        price = float(input("Input product price: "))
 
-except json.JSONDecodeError:
-    print("Error: El archivo JSON esta mal formado.")
-except FileNotFoundError:
-    print("Error: El archivo no existe.")
-except Exception as e:
-    print("Error inesperado: {e}.")
+        if products[i]["id"] == id:
+            print("Product ID already exists.")
+            input("Continue...")
+            break
+        elif products[i]["id"] != id:
+            try:
+                almacen.append({"id": id,"name": name, "price": price})
+                saveProducts()
+                print("Product added successfully.")
+                print(f"ID: {id}\nName: {name}\nPrice: {price}")
+                input("Continue...")
+                break
+            except json.JSONDecodeError:
+                print("Error saving product. Please try again.")
+                input("Continue...")
+                break
+
+def seeProducts():
+    with open(fetch, "r") as file:
+        products = json.load(file)
+        for product in products:
+            print(f"ID: {product['id']} - Name: {product['name']} - Price: {product['price']}")
+    input("Continue...")
+
+def removeProduct():
+    with open(fetch, "r") as file:
+        products = json.load(file)
+    id = int(input("Input product ID to remove: "))
+    for i in range(len(products)):
+        if products[i]["id"] == id:
+            del almacen[i]
+            print("Product removed successfully.")
+            saveProducts()
+            input("Continue...")
+            break
+        else:
+            print("Priduct ID not found.")
+            input("Continue...")
+            break
+
+saveProducts()
+menu = """"
+Products
+
+1. Add product.
+2. see products.
+3. Remove product.
+4. Exit.
+"""
+
+while True:
+    print(menu)
+    op = int(input("Input option: "))
+    if op == 1:
+        addProduct()
+    elif op == 2:
+        seeProducts()
+    elif op == 3:
+        removeProduct()
+    elif op == 4:
+        break
+    else:
+        print("Invalid option.")
---
+++ Taller 6/main.py
@@ -0,0 +1,25 @@
+fjson = "Taller 6/data.json"
+fcsv = "Taller 4/users.csv"
+import json
+import csv
+
+def convert():
+    with open(fcsv, "r") as file:
+        reader = csv.DictReader(file)
+        try:    
+            with open(fjson, "w") as json_file:
+                json.dump(list(reader), json_file, indent=2)
+                print("CSV to JSON conversion completed successfully.")
+                input("continue...")
+        except Exception as e:
+            print(f"Error converting CSV to JSON: {e}")
+        
+while True:
+    print("1. Convert JSON to CSV")
+    op = int(input("Choose an option: "))
+    if op == 1:
+        convert()
+    elif op == 2:
+        break
+    else:
+        print("Invalid option")
